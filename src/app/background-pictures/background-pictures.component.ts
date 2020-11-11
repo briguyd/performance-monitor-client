fb2b7cfe19301a81a58f5300fc4bad33adf7b3c9
Brian DeMaio <bdemaio@skyroad.com>
2020-11-11 00:09:14 -0600

    better animations

--- src/app/background-pictures/background-pictures.component.ts
+++ src/app/background-pictures/background-pictures.component.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
 import {
   trigger,
   state,
   style,
   animate,
   transition,
+  AnimationEvent, keyframes
   // ...
 } from '@angular/animations';
 
@@ -14,6 +15,42 @@ import {
   templateUrl: './background-pictures.component.html',
   styleUrls: ['./background-pictures.component.scss'],
   animations: [
+    // danceAnimation
+    trigger('dancingVillager', [
+      state('normal', style({
+        transform: 'rotate(0deg) scale(1)'
+      })),
+      state('tiltLeft', style({
+        transform: 'rotate(-90deg) scale(1)'
+      })),
+      state('tiltRight', style({
+        transform: 'rotate(90deg) scale(1)'
+      })),
+      state('slightShrink', style({
+        transform: 'scale(.5)'
+      })),
+      state('disappear', style({
+        transform: 'scale(0.01)'
+      })),
+      transition('* => *', [
+        animate('5s')
+      ]),
+      // transition('* => tiltRight', [
+      //   animate('5s')
+      // ]),
+      // transition('* => slightShrink', [
+      //   animate('5s')
+      // ]),
+      // transition('* => disappear', [
+      //   animate('5s')
+      // ]),
+      transition('disappear => *', [
+        animate('5s', keyframes([
+          style({ transform: 'scale(0.1)', offset: 0.1}),
+          style({ transform: 'scale(0.5)', offset: 0.5})
+        ]))
+      ])
+    ])
   ]
 })
 export class BackgroundPicturesComponent implements OnInit {
@@ -24,10 +61,12 @@ export class BackgroundPicturesComponent implements OnInit {
   public count = 0;
   public displayedVillagers = [];
 
-  constructor(private http: HttpClient, private zone: NgZone, private el: ElementRef) { }
+  constructor(private http: HttpClient, private zone: NgZone, private el: ElementRef,
+    private cdRef : ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.getVillagersList().subscribe((villagers) => this.setVillagerList(villagers));
+    setInterval(() => this.updateAnimations(), 5000);
   }
 
   setVillagerList(villagers: any) {
@@ -36,12 +75,40 @@ export class BackgroundPicturesComponent implements OnInit {
     this.displayVillagers();
   }
 
+  updateAnimations() {
+    for (const row of this.displayedVillagers) {
+      for (const villager of row) {
+        const rand = Math.random();
+        if (rand > 0.80) {
+          villager.state = 'tiltLeft'
+        } else if (rand > .6) {
+          villager.state = 'tiltRight';
+        } else 
+        if (rand > 0.5) {
+          villager.state = 'normal';
+        } 
+        else if (rand > .3) {
+          villager.state = 'disappear';
+        }
+      }
+    }
+  }
+
+  onAnimationEvent(event: AnimationEvent, villager) {
+
+    if (event.toState === 'disappear') {
+      villager.image = this.getRandomVillager();
+      this.cdRef.detectChanges();
+    }
+  }
+
   displayVillagers() {
     let villagerIndex = 0;
+    // this.displayedVillagers = [[{image: this.getRandomVillager(), state: 'hiddenVillager'}]];
     for (let i = 0; i < (this.backgroundHost.nativeElement.offsetHeight / 64) - 1; i++) {
       const row = [];
       for (let j = 0; j < (this.backgroundHost.nativeElement.offsetWidth / 64) - 1; j++) {
-        row.push(this.getRandomVillager());
+        row.push({image: this.getRandomVillager(), state: 'hiddenVillager'});
         villagerIndex++;
       }
       this.displayedVillagers.push(row);
