410c98ebe0e0c9f73a844644b4afa5532e801103
Brian DeMaio <bdemaio@skyroad.com>
2020-11-28 19:50:05 -0600

    keep last used connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { interval, Subscription } from 'rxjs';
 })
 export class AppComponent {
 
+  private storageKey = 'hwmon-connection';
   connected = false;
   host = 'localhost';
   port = 8086;
@@ -21,18 +22,38 @@ export class AppComponent {
 
   }
 
+  ngOnInit() {
+    // To retrieve data from localstorage
+    const storedSettings = JSON.parse(localStorage.getItem(this.storageKey));
+
+    if (storedSettings && storedSettings.host && storedSettings.port) {
+      this.port = storedSettings.port;
+      this.host = storedSettings.host;
+      this.connected = true;
+      this.connectToStatus();
+      console.log(this.port, this.host);
+    }
+  }
+
+  disconnectFromMonitor() {
+    this.connected = false;
+    localStorage.removeItem(this.storageKey);
+  }
+
   connectToStatus() {
     this.hwMon.getHWStatus(this.host, this.port).subscribe((result) => this.result = result);
 
     const source = interval(10000);
     this.subscription = source.subscribe(() => this.hwMon.getHWStatus(this.host, this.port).subscribe((result) => this.result = result));
     this.connected = true;
+    
+    localStorage.setItem(this.storageKey, JSON.stringify({ host: this.host, port: this.port }));
   }
 
   getComponentByIcon(iconName: string) {
     if (this.result?.Children && this.result.Children.length > 0) {
       for (const child of this.result.Children[0]?.Children) {
-        
+
         if (child.ImageURL === iconName) {
           return child;
         }
@@ -43,7 +64,7 @@ export class AppComponent {
   getComponentByName(name: string) {
     if (this.result?.Children && this.result.Children.length > 0) {
       for (const child of this.result.Children[0]?.Children) {
-        
+
         if (child.Text === name) {
           return child;
         }
